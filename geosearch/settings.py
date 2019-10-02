7a04fcd09e7688a656e9bdbe0c88edeb9c52b1d2
Maurizio Martin <macl34@msn.com>
2019-10-02 11:37:23 +0200

    settings

--- geosearch/settings.py
+++ geosearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f*1f(djlx(uyq)2nl1ugvok419n3tgkx%&qada!lw_!s4_b%ia'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '134.209.202.96']
 
 
 # Application definition
