ac39f8ee349a47d14fa6e9caa56c1e632d23c58c
Maurizio Martin <macl34@msn.com>
2019-10-17 14:26:05 +0200

    sett

--- geosearch/settings.py
+++ geosearch/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -121,3 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 #STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
